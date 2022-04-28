2004d381a2175c5e8d93f18140b30e8cbe332efa
zG <22377978+zenGator@users.noreply.github.com>
2022-04-28 16:56:10 -0400

    many more comments and better for sharing

--- doit.ps1
+++ doit.ps1
@@ -1,39 +1,151 @@
-Import-Module sharepointpnppowershellonline
-$sourceURL="https://[redacted].sharepoint.com/sites/[source]"
-$destURL="https://[redacted].sharepoint.com/sites/[dest]"
+#copySPsite
+#20220427:zG
+#
+#
+<#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ # This will copy a set of SharePoint pages; e.g., to build new client project from template
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#>
+
+[CMDletBinding()]
+#param ([Parameter(Mandatory=$true)][string]$inFile)
+param ( [Parameter(Mandatory=$true)][string]$sourceName = 'SAQDTemplate',
+        [Parameter(Mandatory=$true)][string]$destName,
+        #[switch]$overwrite,
+        #$dirsToMonitor = @() ,
+        #[switch]$debugging ,
+        #[int]$skipto = 0,
+        #[string]$errors = $pwd.Path +"\"+ 'error_log.txt',
+        #[int]$checkinMins
+        [switch]$myVerbose
+         )
+
+#NB:  if I remember correctly, the param bit above has to come first
+
+
+# LEGACY:  Import-Module sharepointpnppowershellonline
+Import-Module PnP.PowerShell   # we need this to interact with SharePoint
+                               # ref: https://pnp.github.io/powershell/cmdlets/Copy-PnPFolder.html
+
+$sourceURL='https://sylint.sharepoint.com/sites/' + $sourceName
+$destURL='https://sylint.sharepoint.com/sites/' + $destName
+
+
+<# test for existence of source & destination URLs
+    if either doesn't exist, warn and abort
+ #>
+$status=try {  #check for destination
+    (Invoke-WebRequest -Uri $destURL -UseBasicParsing -DisableKeepAlive -Method Head).statuscode 
+    } 
+    catch [Net.WebException] {[int]$_.Exception.Response.StatusCode}
+if ($status -ne 200) {
+    write 'error:  can''t access destination; aborting'
+    exit
+    }
+
+$status=try {  #check for source
+    (Invoke-WebRequest -Uri $sourceURL -UseBasicParsing -DisableKeepAlive -Method Head).statuscode 
+    } 
+    catch [Net.WebException] {[int]$_.Exception.Response.StatusCode}
+if ($status -ne 200) {
+    write 'error:  can''t access source; aborting'
+    exit
+    }
+
+
+<# we will build a list of the temporary files created from the template (source); 
+   these will then be pushed to the destination site
+ #>
 $tempFiles=[System.Collections.ArrayList]@()
-#$myuid="z***g@[redacted].com"
-#$mypass=read-host -Prompt "pwd: " -AsSecureString
-#$Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminUPN, $AdminPassword
-Connect-PnPOnline -Url $sourceURL -useweblogin
-#Get-PnPListItem -List sitepages
+
+<# this method of building creds to authenticate to SP didn't work very well (at all)
+ #$myuid="[redacted]@[company].com"
+ #$mypass=read-host -Prompt "pwd: " -AsSecureString
+ #$Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminUPN, $AdminPassword
+ #>
+
+# test to see if we are connected where we need to be
+$currConn=Get-PnPConnection
+if ($currConn.url -ne $sourceURL) {
+    write 're-connecting to source'
+    Connect-PnPOnline -Url $sourceURL -useweblogin 3> null
+    }
+$currConn=Get-PnPConnection
+if ($currConn.url -ne $sourceURL) {
+    write 'We''re having problems establishing a connection to SharePoint.  Try closing this PoSh session and starting anew.'
+    write 'It could be a permissions issue.  It could be that you need to authenticate to SharePoint from your browser before using this script.'
+    write 'In any case, we''re aborting.  Sorry.'
+    exit
+    }
+
+#let's see what pages need to be copied
 $pages=Get-PnPListItem -List sitepages
-#$pages=Get-PnPListItem -List siteassets
-#put this next one back
-#$pageName = $pages[0].FieldValues["FileLeafRef"]
 
-#Connect-PnPOnline -Url https://contoso.sharepoint.com -Credentials (Get-Credential)
-Connect-PnPOnline -Url $sourceURL -UseWebLogin  #-Credentials (Get-Credential)
+<#ToDo: similar process for copying the various icons
+    perhaps, instead, use Copy-PnPFolder
+ #>
+#$docs=Get-PnPListItem -List Documents
 
-for ($i=2; $i -lt 5; $i++) { 
-    write-host $i $pages[$i].fieldvalues["FileLeafRef"]
-    $pageName = $pages[$i].FieldValues["FileLeafRef"]
-    $ServerRelativeUrl=(Get-PnPWeb).ServerRelativeUrl
-    $file = Get-PnPFile -Url "$ServerRelativeUrl/sitePages/$pageName"
-    $tempFile = [System.IO.Path]::GetTempFileName()
-    Export-PnPClientSidePage -Force -Identity $pageName -Out $tempFile
-    $null=$tempFiles.add($tempFile)
+
+if ($myVerbose) {  #there's other places where we can be more or less verbose; but don't use $verbose in this code; that's a switch in the PnP.Powershell module
+    write 'We''ll be copying these pages: '
+    $pages.fieldvalues| foreach-object {write-host ([string]$_.ID),$_.FileLeafRef}
+    write ('total: ' + $pages.Count)
+    }
+
+#write 'debugging:  end reached'
+#exit 256
+
+# here's the heart of this script
+for ($i=0; $i -lt $pages.Count; $i++) { #for each of the sitepages listed above
     
-    #Connect-PnPOnline -Url $destURL -UseWebLogin
-    #Apply-PnPProvisioningTemplate -Path $tempFile
-    }
-#then set the new Home.aspx as the site homepage
-#various graphics are not in place
-#also, the News webpart is not in place
-$tempFiles
+    write-host -NoNewline 'copying '    
+    write-host -nonewline $pages[$i].fieldvalues["FileLeafRef"] ' . . . '
+        <# fieldvalues["FileLeafRef"] is the page name as it shows in the SitePages list
+           fieldvalues["Title"] is shown in the default list display
+           this is one of those places we could be less verbose
+         #>
 
-Connect-PnPOnline -Url $destURL -UseWebLogin
-for ($i=0; $i -lt $tempFiles.Count; $i++) { 
-    write-host $i $tempFiles[$i] "=>" $pages[$i+1].fieldvalues["FileLeafRef"]
-    Apply-PnPProvisioningTemplate -Path $tempFiles[$i]
+    $pageName = $pages[$i].FieldValues["FileLeafRef"]  #as noted above, this is the page's filename (typically "[blah].aspx"
+    $ServerRelativeUrl=(Get-PnPWeb).ServerRelativeUrl
+    $file = Get-PnPFile -Url "$ServerRelativeUrl/sitePages/$pageName"  #get the sitepage
+    $tempFile = [System.IO.Path]::GetTempFileName()    #here we assign a name for the tempfile
+    
+    #LEGACY: Export-PnPClientSidePage -Force -Identity $pageName -Out $tempFile
+    Export-PnPPage -Force -Identity $pageName -Out $tempFile  #save the sitepage as tempfile
+    
+    $null=$tempFiles.add($tempFile)   #pop onto our list of temp files
+    write-host 'done'   # a little feedback for the UX
     }
+
+#ToDo:  see if the the News webpart is in place
+
+if ($myVerbose) {
+    write-host "These are the temporary files which will be copied over to ${destName}:"
+    $tempFiles  #list them out for UX
+    }
+
+Connect-PnPOnline -Url $destURL -UseWebLogin 3> $null  # now we need to connect to the destination
+<#test to see that we've connected successfully
+ #>
+if ($currConn.url -ne $desturl) {
+    write 'We''re having problems establishing a connection to SharePoint.  Try closing this PoSh session and starting anew.'
+    write 'It could be a permissions issue.  It could be that you need to authenticate to SharePoint from your browser before using this script.'
+    write 'This is unexpected because we were able to connect to the source.'
+    write 'In any case, we''re aborting.  Sorry.'
+    exit
+    }
+
+<#ToDo:  consider testing to see that the number of tempfiles is equal to the # of sitepages 
+ #>
+for ($i=0; $i -lt $tempFiles.Count; $i++) { #for each of the temp files
+    write-host $i $tempFiles[$i] "=>" $pages[$i+1].fieldvalues["FileLeafRef"]  #again, some feedback for the UX
+    
+    #LEGACY:  Apply-PnPProvisioningTemplate -Path $tempFiles[$i]
+    Invoke-PnPSiteTemplate -Path $tempFiles[$i]
+    }
+
+write-host "Template copy of $sourceName pages to $destName is complete."
+write-host "Next step:  copy items in icons&such to $destName and enjoy your new project."
+#ToDo:  see if copying icons&such to the new location might make the graphics auto-magically appear; if so, the above line is unnecessary
+write-host 'Also, you will want to point the links in Overall-Status.aspx to the local/project pages instead of the template.'
+write-host 'Finally, make sure the spreadsheet section of each Requirements page references the spreadsheet in the client''s project OneDrive, and not the Template.'
